93d0a4e910e642111852e76d0782dd8a4df91d3a
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-08-07 22:05:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+// 1-misol
+
+// let arr = [1, 2, 3, 4];
+// let num = [];
+// let res = arr.forEach(function(v) {
+//     num.push(v * 2);
+// });
+// console.log(num);
+
+// 3-misol
+
+// let arr = [1, 2, 3, 4, 5];
+// let res = [];
+// arr.forEach(function(v) {
+//     res.push(v + 5);
+// })
+// console.log(res);
+
+//
+
+// 1-misol
+
+// let arr = [1, 2, 3, 4, 5];
+// let res = arr.map(function(v) {
+//     return v * 2;
+// })
+// console.log(res);
+
+// 2-misol
+
+// let arr = ['salom', 'xayr'];
+// let res = [];
+// arr.forEach(function(v) {
+//     res.push(v.toUpperCase())
+// });
+// console.log(res);
+
+// 2-misol
+
+// let arr = ['gilos', 'behi', 'olma', 'tarvuz'];
+// let res = arr.map(function(v) {
+//     return v.toUpperCase();
+// })
+// console.log(res);
+
+// 3-misol
+
+// let arr = [1, 2, 3, 4, 5];
+// let res = arr.map(function(v){
+//     return v + 5;
+// })
+// console.log(res);
+
+// 1-misol
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// let res = arr.filter(function(v) {
+//     return v % 2 == 0;
+// })
+// console.log(res);
+
+// 2-misol
+
+// let arr = ['apple', 'banana', 'cherry', 'date'];
+// let res = arr.filter(function(v) {
+//     return v.length > 5;
+// })
+// console.log(res);
+
+// 3-misol
+
+// let arr = [12, 17, 18, 19, 21];
+// let res = arr.filter(function(v) {
+//     return v > 18;
+// })
+// console.log(res);
+
+
+// 1-misol
+
+// let arr = [4, 9, 11, 2, 18];
+// let res = arr.find(function(v) {
+//     return v > 10;
+// })
+// console.log(res);
+
+// some/every metodlari
+
+// 1-misol
+
+// let arr = [-1, -2, 3, -4];
+// let res = arr.some(function(v) {
+//     return v > 0;
+// })
+// console.log(res);
+
+// 2-misol
+
+// let arr = ['cat', 'dog', 'elephant'];
+// let res = arr.some(function(v) {
+//     return v.length > 5;
+// })
+// console.log(res);
+
